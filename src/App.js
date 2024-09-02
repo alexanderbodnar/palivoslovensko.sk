17aaae8d209baf9eb088bfa31e9ff8d9e8c0fa61
Ivo Bubeňko <ivo.bubenko@student.tuke.sk>
2024-09-02 20:31:59 +0200

    added footer, main section and navbar, support for multiple languages, basic api request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import { getDataWithParams } from "./API/monthlyData";
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect } from "react";
+import NavigationBar from "./Components/NavigationBar";
+import FooterSection from "./Components/FooterSection";
+import MainSection from "./Components/MainSection";
+import { useTranslation } from "react-i18next";
+import "./i18n";
 
 function App() {
+  // useEffect(() => {
+  //   const fetchDataFromApi = async () => {
+  //     console.log(await getDataWithParams("202452"));
+  //   };
+  //   fetchDataFromApi();
+  // }, []);
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <text>Alex je frajer</text>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar t={t} i18n={i18n} />
+      <MainSection />
+      <FooterSection />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "D5DFE5", // Light theme primary color
+        secondary: "C9B1BD", // Light theme secondary color
+        darkPrimary: "7F9172", // Dark theme primary color
+        darkSecondary: "567568", // Dark theme secondary color
+      },
+    },
   },
   plugins: [],
 };
