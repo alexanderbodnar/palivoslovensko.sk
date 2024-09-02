7e73eb6e58796272b2b7b8961408e201326fc2b8
Alex <alexander.bodnar@student.tuke.sk>
2024-09-02 12:42:33 +0200

    fixing <br> bug

--- update_readme.py
+++ update_readme.py
@@ -10,17 +10,10 @@ with open(readme_path, "r") as readme_file:
 with open(changelog_path, "r") as changelog_file:
     changelog_content = changelog_file.read()
 
-# Split the changelog by entries
-changelog_entries = re.split(r'(● \d{2}\.\d{2}\.\d{4})<br>', changelog_content)
-
-# Reverse the order of the entries
-reversed_changelog_entries = changelog_entries[1:][::-1]
-reversed_changelog = ''.join(reversed_changelog_entries)
-
 # Insert the latest changelog into the README.md
 readme_updated = re.sub(
     r'(<!-- changelog-start -->)(.*)(<!-- changelog-end -->)',
-    rf'\1\n{reversed_changelog}\n\3',
+    rf'\1\n{changelog_content}\n\3',
     readme_content,
     flags=re.DOTALL
 )
